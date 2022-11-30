efbbc04f6158774056113381eeaf7f8dec3027c5
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-11-30 11:46:47 -0300

    exercicios realizados no dia 30/11/2021

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,50 +1,60 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { handleHome } from "../Router/cordinator";
 import styled from "styled-components";
 
-export default function Cadastro (props){
-  const[formulario, setFormulario]=useState({name:"", url:"", price:""})
+export default function Cadastro(props) {
+  const { frutas, setFrutas } = props;
+  const [formulario, setFormulario] = useState({ name: "", url: "", price: "" })
   const navigate = useNavigate();
 
-  const onChangeInputs=(event)=>{
-    const {name, value}= event.target
-    setFormulario({...formulario, [name]:value})
+  const onChangeInputs = (event) => {
+    const { name, value } = event.target
+    setFormulario({ ...formulario, [name]: value })
   }
-  const handleClick = (event)=>{
-    event.preventDefault()
-    console.log(formulario);
-  }
- 
- 
-  return(
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const novaFruta = {
+      ...formulario,
+      id: Date.now()
+    };
+
+    novaFruta.price = Number(novaFruta.price);
+
+    setFrutas([...frutas, novaFruta]);
+    console.log(novaFruta)
+  };
+
+  return (
     <CadastroContainer>
       <h1>Cadastro</h1>
       <button onClick={() => handleHome(navigate)}>Voltar</button>
-    <FormContainer onSubmit={handleClick}>
-      <Input
-      name="name"
-      value={formulario.name}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="Nome da Fruta"
-      />
-      <Input 
-      name="url"
-      value={formulario.url}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="url da imagem"
-      />
-      <Input
-      name="price"
-      value={formulario.price}
-      onChange={onChangeInputs}
-      type="number"
-      placeholder="Preço da Fruta"
-      />
-      <button> Cadastrar </button>
-    </FormContainer>
+      <FormContainer onSubmit={handleClick}>
+        <Input
+          name="name"
+          value={formulario.name}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="Nome da Fruta"
+        />
+        <Input
+          name="url"
+          value={formulario.url}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="url da imagem"
+        />
+        <Input
+          name="price"
+          value={formulario.price}
+          onChange={onChangeInputs}
+          type="number"
+          placeholder="Preço da Fruta"
+        />
+        <button> Cadastrar </button>
+      </FormContainer>
     </CadastroContainer>
   )
 }
